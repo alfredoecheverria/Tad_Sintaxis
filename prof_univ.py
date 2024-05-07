3a5008b0f8bc600037341b756f79b6d06cb81d0b
alfredoecheverria <alfredoecheverriapalma@gmail.com>
2024-05-06 22:01:47 -0300

    inicial

---
+++ prof_univ.py
@@ -0,0 +1,22 @@
+""" Un profesor universitario tiene una agenda donde guarda la información de las tutorías con
+sus estudiantes: Nombre del estudiante, carrera, correo electrónico, fecha de la tutoría y hora
+de la tutoría. Se deberá desarrollar una aplicación, utilizando las estructuras de datos que
+sean necesarias. Se desea tener un menú con las siguientes opciones:
+a) Agregar/modificar/eliminar tutoría: Permite agregar, modificar o eliminar una tutoría
+con un estudiante, utilizando su nombre como referencia.
+b) Listado de tutorías: Muestra un listado completo de todas las tutorías programadas,
+incluyendo los detalles de cada estudiante y la información de la tutoría.
+c) Reagendar tutoría: Permite reprogramar una tutoría de un estudiante específico para el
+mismo día y horario de la semana siguiente, facilitando la organización de las reuniones.
+d) Eliminar tutorías de una carrera: Permite eliminar todas las tutorías programadas para
+estudiantes de una carrera específica, liberando el tiempo del profesor para atender a
+estudiantes de otras carreras.
+e) Generar una lista de estudiantes por día: Crea una lista de estudiantes que tienen tutoría
+programada en un día específico, incluyendo sus nombres y carreras, para una mejor
+planificación de la jornada del profesor.
+f) Generar una pila con los estudiantes que tengan fecha de tutoría en el último mes. Listar
+la pila cada vez se elija esta opción (tengan cuidado de no dejar la pila vacía al terminar
+de listar). """
+
+
+
